0f970f6b4f13b9c3728c1f49ee06f45fb9e679a2
ranthy <realthingshakes25@gmail.com>
2017-05-01 00:35:44 +0900

    first practice

---
+++ learn_elixir/test_code/lib/test_code.ex
@@ -0,0 +1,23 @@
+defmodule TestCode do
+  @moduledoc """
+  Documentation for TestCode.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestCode.hello
+      :world
+
+  """
+  def hello do
+    #IO.puts "Hello"
+    :world
+  end
+
+  def main do
+    IO.puts "Hello"
+  end
+end
---
+++ learn_elixir/test_code/test/test_code_test.exs
@@ -0,0 +1,8 @@
+defmodule TestCodeTest do
+  use ExUnit.Case
+  doctest TestCode
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
