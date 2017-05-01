43428db868616dba8e8d872269599f7d161db5f2
ranthy <realthingshakes25@gmail.com>
2017-05-02 01:00:42 +0900

    標準入出力などの練習

--- learn_elixir/test_code/lib/test_code.ex
+++ learn_elixir/test_code/lib/test_code.ex
@@ -17,7 +17,15 @@ defmodule TestCode do
     :world
   end
 
+  def calc_div(num) do
+    rem num, 2
+  end
+
   def main do
-    IO.puts "Hello"
+    input = IO.gets "Input number: "
+    # |>...前の関数の結果を次の関数の第一引数として渡す
+    num = input |> String.replace("\n", "")
+    calc_div = fn a -> rem a, 2 end
+    IO.puts calc_div.(String.to_integer(num))
   end
 end
